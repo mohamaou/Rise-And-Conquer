1fcfb95880fae04626394bb18009daa78d9bc1b8
mohamaou <92312247+mohamaou@users.noreply.github.com>
2026-01-31 18:47:32 +0000

    First Push

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Rise _ Conquer Unity-0.1.0";
+const contentToCache = [
+    "Build/Rise-And-Conquer.loader.js",
+    "Build/Rise-And-Conquer.framework.js",
+    "Build/Rise-And-Conquer.data",
+    "Build/Rise-And-Conquer.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
